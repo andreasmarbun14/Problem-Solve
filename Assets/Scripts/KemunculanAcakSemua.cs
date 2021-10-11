3b0b16edaef7341b3dfa24cdbcf3128feb3e1cee
andreasmarbun14 <andreasmarbun0@gmail.com>
2021-10-12 04:09:26 +0700

    Commit Files

---
+++ Assets/Scripts/KemunculanAcakSemua.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TreeEditor;
+
+public class KemunculanAcakSemua : MonoBehaviour
+{
+    //  Menambahkan Game Objek Kotak
+    public GameObject kotak;
+    public float Anjay_Kiri, Anjay_Kanan, Anjay_Bawah, Anjay_Atas;
+
+    void Start()
+    {
+        // Setting Spawn Kotak
+        StartCoroutine(Memunculkan_Kotak());
+    }
+
+    // Mengatur Letak Spawn Kotak
+    IEnumerator Memunculkan_Kotak()
+    {
+        if (transform.childCount < 10)
+        {
+            float AcakX = Random.Range(Anjay_Kiri, Anjay_Kanan);
+            float AcakY = Random.Range(Anjay_Bawah, Anjay_Atas);
+
+            Instantiate(kotak, new Vector2(AcakX, AcakY), Quaternion.identity);
+
+        }
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(Memunculkan_Kotak());
+    }
+}   
+
+// :D  // :))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Andreas Marbun :D
---
+++ Assets/Scripts/PengaturanSkors.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class PengaturanSkors : MonoBehaviour
+{
+    #region singleton
+
+    private static PengaturanSkors Input_Skor;
+
+    public static PengaturanSkors Instance
+    {
+        get
+        {
+            if (Input_Skor == null)
+                Input_Skor = FindObjectOfType<PengaturanSkors>();
+            return Input_Skor;
+        }
+    }
+
+    #endregion
+
+    private static int Nilai_Windah;
+
+    public Text Nilai_Ku;
+
+    // Method Ketika Objek Trigger
+    private void Awake()
+    {
+        Nilai_Windah = 0;
+    }
+
+    private void Update()
+    {
+        Nilai_Ku.text = "Nilai-Ku : " + Nilai_Windah;
+    }
+
+    public void SkorSings()
+    {
+        Nilai_Windah++;
+    }
+
+    // :D
+    // Maafkeun Kalau Suka Sembarangan Isi Scriptnya, Soalnya Saya Trauma Di-Copas Kayak Chapter-2 :))
+}
