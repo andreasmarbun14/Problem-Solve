fa875023ba7ab22004d008ccd818d3626fd65bce
andreasmarbun14 <andreasmarbun0@gmail.com>
2021-10-12 00:35:43 +0700

    Commit FIles
    Menginput Project 6

---
+++ Assets/Scripts/KotakPrefabs.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEditor;
+
+public class KotakPrefabs : MonoBehaviour
+{
+    public float Darah = 50f;
+
+    public UnityAction<GameObject> YourDeath = delegate { };
+
+    private bool _isHit = false;
+
+    void OnDestroy()
+    {
+        if (_isHit)
+        {
+            YourDeath(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.GetComponent<Rigidbody2D>() == null) return;
+
+        // Game Tag Bola
+        if (col.gameObject.tag == "Bola")
+        {
+            _isHit = true;
+            Destroy(gameObject);
+        }
+        else if (col.gameObject.tag == "Tembok")
+        {
+            // Menghintung Damage Yang Diterima
+            float KenaDamageDanMental = col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10;
+            Darah -= KenaDamageDanMental;
+
+            if (Darah <= 0)
+            {
+                _isHit = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Memunculkan.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Memunculkan : MonoBehaviour
+{
+    public GameObject Kotak_Kubus_Ku;
+    public float Anjay_Kiri, Anjay_Kanan, Ohh_Atas, Ohh_Bawah;
+
+    void Start()
+    {
+        Lah_Kok_TibaTibaAda();
+    }
+    void Lah_Kok_TibaTibaAda()
+    {
+        int Mau_BerapaBanyakYa = Random.Range(15, 30);
+        for (int i = 0; i < Mau_BerapaBanyakYa; i++)
+        {
+            float randomX = Random.Range(Anjay_Kiri, Anjay_Kanan);
+            float randomY = Random.Range(Ohh_Atas, Ohh_Bawah);
+            Instantiate(Kotak_Kubus_Ku, new Vector2(randomX, randomY), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
