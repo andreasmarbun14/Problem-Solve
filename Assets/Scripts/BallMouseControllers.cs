d39b7283a761efca3960f345bfc2f773b97308cb
andreasmarbun14 <andreasmarbun0@gmail.com>
2021-10-11 23:20:13 +0700

    Commit Files

---
+++ Assets/Scripts/BallMouseControllers.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class BallMouseControllers : MonoBehaviour
+{
+    // Method Player Body
+    private Rigidbody2D Pemainrigidbody2D;
+
+    // Method Camera
+    public Camera Kamera_Utama;
+
+    // Method Kecepatan
+    public float Kecepatan;
+
+    
+    private void Start()
+    {
+        // Mendapatkan Method Rigibody
+        Pemainrigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        // Mengatur Kecepatan Untuk Menuju Mouse Kursor
+        Vector3 MousePlayer = Kamera_Utama.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+        Vector3 angle = MousePlayer - transform.position;
+        // Andreas Marbun
+        // Scripts
+        // Method Jika Mouse Bergerak Maka Benda Akan Mengikuti Kursor Tersebut
+        if (Vector3.Distance(MousePlayer, transform.position) > 0.5)
+
+        {
+            transform.Translate(angle.normalized * Kecepatan * Time.deltaTime);
+        }
+
+
+    }
+
+    // :D
+    // :)
+    // Akhirnya Bisa :D
+}
